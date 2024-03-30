8cf5c28136b96d875b81c2f133b8bfad158c2ec7
Kabina G.C <g.c.kabina@gmail.com>
2024-03-30 20:55:02 +0545

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2023 <a href="https://www.Kathmandu BernHardt college.com">kathmanduBernHardt</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
---
+++ readme.txt
@@ -0,0 +1,7 @@
+username  himal_karkee
+password  admin1234
+
+
+voter id:LdoPSfW9cJY1ajE
+password:himalkarki
+
